76bed364a88d17ea2f0a05876de2f1f14d356909
Inácio Oliveira <inacioigne@gmail.com>
2026-03-31 16:41:20 -0400

    31/03/2026

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     minimumCacheTTL: 2678400,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
