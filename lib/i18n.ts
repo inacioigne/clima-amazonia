c4e4b9c5a515c7a06f3b979db1efa8f4a5754769
Inácio Oliveira <inacioigne@gmail.com>
2026-07-14 14:47:31 -0400

    edition 0708

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,4 +1,4 @@
-import type boletim from "@/data/boletim/2026/0603.json";
+import type boletim from "@/data/boletim/2026/0708.json";
 import type anomaly from "@/data/boletim/2026/0513.json";
 import type messages from "@/data/i18n/pt.json"
 
